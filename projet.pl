f00a317c17fc3ae222ae04f011ccd34580fe0caa
tholstein <59557596+tholstein@users.noreply.github.com>
2021-03-11 11:44:21 +0100

    debut branche affichage grille

--- projet.pl
+++ projet.pl
@@ -1,4 +1,4 @@
-:- dynamic mur/3, 
+:- dynamic mur/3,
 joueur/1,                     %determine la personne qui doit jouer
 compteurJ1/1, compteurJ2/1.   %Contient le NbMur restant par joueur
 %score/1, compteur/1,  nbtour/1.
@@ -13,7 +13,7 @@ joueur(1).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                           FONCTIONS PRINCIPALES                                 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-demarrer :- 
+demarrer :-
     nl,
     write('Bienvenue dans Quoridor ! '), nl,
     instructions,
@@ -21,7 +21,7 @@ demarrer :-
 
 instructions :-
   nl,
-  write('Inserer ici les regle du jeu.'), nl, 
+  write('Inserer ici les regle du jeu.'), nl,
   write('Inserer ici les regle du jeu.'), nl,nl,
   write('Pour pouvoir jouer, entrez les differentes commandes suivantes en respectant la syntax Prolog (pas de majuscule et avec un point a la fin de la commande).'), nl,
   write('Les differentes commandes du jeu sont :'), nl,
@@ -58,11 +58,11 @@ tourJoueur(1,m) :-                          %vérifie si il reste des murs à po
   write('plus de mur'),
   deplacer.
 
-tourJoueur(1,m) :- 
-  poserMur(1), 
+tourJoueur(1,m) :-
+  poserMur(1),
   compteur(compteurJ1).
 
-tourJoueur(1,d) :- 
+tourJoueur(1,d) :-
   deplacer.
 
 tourJoueur(2,m) :-                          %vérifie si il reste des murs à poser
@@ -70,11 +70,11 @@ tourJoueur(2,m) :-                          %vérifie si il reste des murs à po
   write('plus de mur'),
   deplacer.
 
-tourJoueur(2,m) :- 
-  poserMur(1), 
+tourJoueur(2,m) :-
+  poserMur(1),
   compteur(compteurJ2).
 
-tourJoueur(2,d) :- 
+tourJoueur(2,d) :-
  deplacer.
 
 
@@ -94,46 +94,46 @@ afficheGrilleJeu :- grilleJeu(G), printGrid(G). %affichage de la grille de jeu
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                              GESTION DES MURS                                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-poserMur(1):- 
+poserMur(1):-
   write('position ?'),
   read(Position),
   write('sens ?'),
   read(Sens),
   poseMur(Position,Sens).
 
-poserMur(2):- 
+poserMur(2):-
   write('Impossible de poser un mur ici. Recommencez !'),nl,
   poserMur(1).
 
 poseMur(Position,Sens) :-     %si il y a déjà un mur de placé ici
-  mur(Position,Sens,_), 
+  mur(Position,Sens,_),
   poserMur(2).
 
 poseMur(Position,h) :-        %si on coupe un mur
-  coupeMur(Position, v), 
+  coupeMur(Position, v),
   poserMur(2).
 
-poseMur(Position,v) :- 
-  coupeMur(Position, h), 
+poseMur(Position,v) :-
+  coupeMur(Position, h),
   poserMur(2).
 
-poseMur(Position,v) :- 
+poseMur(Position,v) :-
   \+mur(Position,v,_),                                    %on vérifie qu'il n'y ait pas déjà un mur
-  \+coupeMur(Position, h),                                %on vérifie qu'on ne coupe pas un mur 
-  Position2 is Position +10, \+mur(Position2,v,_),        %on vérifie qu'il n'y ait pas de mur en dessous  
+  \+coupeMur(Position, h),                                %on vérifie qu'on ne coupe pas un mur
+  Position2 is Position +10, \+mur(Position2,v,_),        %on vérifie qu'il n'y ait pas de mur en dessous
   assert(mur(Position, v, debut)),
   Position2 is Position +10,
   assert(mur(Position2, v, fin)).
 
-poseMur(Position,h) :- 
+poseMur(Position,h) :-
   \+mur(Position,h,_),                                    %on vérifie qu'il n'y ait pas déjà un mur
-  \+coupeMur(Position, v),                                %on vérifie qu'on ne coupe pas un mur    
-  Position2 is Position +1, \+mur(Position2,h,_),         %on vérifie qu'il n'y ait pas de mur à droite  
+  \+coupeMur(Position, v),                                %on vérifie qu'on ne coupe pas un mur
+  Position2 is Position +1, \+mur(Position2,h,_),         %on vérifie qu'il n'y ait pas de mur à droite
   assert(mur(Position, h, debut)),
   Position2 is Position +1,
   assert(mur(Position2, h, fin)).
 
-coupeMur(Position, Sens) :-                               %on vérifie qu'on ne coupe pas un mur  
+coupeMur(Position, Sens) :-                               %on vérifie qu'on ne coupe pas un mur
   mur(Position, _, debut).
 
 compteur(compteurJ1) :-                                      %compteur de mur restant pour le joueur 1
@@ -159,35 +159,35 @@ compteur(compteurJ2) :-                                    %compteur de mur rest
 
 
 %%%% AFFICHAGE DU RESULTAT %%%%
-printGrid(G) :- 
+printGrid(G) :-
     printHSep(3),
     printGrid(G,1,1).
-  
-  printGrid(G,L,6) :- 
+
+  printGrid(G,L,6) :-
     !,
     printHSep(L),
     L1 is L + 1,
     printGrid(G,L1,1).
-  
+
   printGrid([],_,_) :- !.
-  
-  
-  printGrid([T|Q],L,C) :- 
+
+
+  printGrid([T|Q],L,C) :-
     write(T),
     printVSep(C),
     C1 is C + 1,
     printGrid(Q,L,C1).
-  
-  
-  printVSep(C) :- 
+
+
+  printVSep(C) :-
     write(' ').
-  
+
   printVSep(_) :- write(',').
-  
-  
-  printHSep(L) :- 
+
+
+  printHSep(L) :-
     nl, write(''), nl,
     write(' ').
-  
+
   printHSep(_) :-
     nl, write('|').
